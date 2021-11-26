7c076a475fc3b7ba1c1f5152a1cdb53a095ef3a1
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-11-26 17:18:27 +0300

    Index action returns json with projects

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,8 +1,38 @@
 class ProjectController < ApplicationController
   def index
-    @message = { message: 'ok' }
     respond_to do |format|
-      format.json { render json: @message }
+      format.json { render json: all_projects }
     end
+  end
+
+  private
+
+  def all_todos_by_project(project)
+    todos = []
+
+    project.todos.each do |todo|
+      todos << {
+        id: todo.id,
+        text: todo.text,
+        isCompleted: todo.isCompleted
+      }
+    end
+
+    todos
+  end
+
+  def all_projects
+    projects = Project.includes(:todos).references(:todos).select(:id, :title)
+
+    json = []
+    projects.each do |project|
+      json << {
+        id: project.id,
+        title: project.title,
+        todos: all_todos_by_project(project)
+      }
+    end
+
+    json
   end
 end
